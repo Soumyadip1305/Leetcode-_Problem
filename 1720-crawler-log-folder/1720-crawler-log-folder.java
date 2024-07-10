8c2eb51e350dc6fd41e6fa54e7dff4ec5118cda9
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2024-07-10 08:55:28 +0530

    Time: 1 ms (99.42%), Space: 42.2 MB (26.85%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.java
+++ 1720-crawler-log-folder/1720-crawler-log-folder.java
@@ -1,14 +1,20 @@
 class Solution {
     public int minOperations(String[] logs) {
-         int cnt = 0;
+        int cnt = 0;
         for (String log : logs) {
-            if (log.equals("./")) continue;
-            else if (log.equals("../")) {
-                if (cnt > 0) cnt--;
-                else cnt=0;
-            } else cnt++;
+            switch (log) {
+                case "./":
+                    break;
+                case "../":
+                    if (cnt > 0) {
+                        cnt--;
+                    }
+                    break;
+                default:
+                    cnt++;
+                    break;
+            }
         }
-
         return cnt;
     }
 }
